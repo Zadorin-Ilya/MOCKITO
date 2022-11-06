94bdeee2c1b49e5de794a5abf933df443399bf26
Илья Задорин <zadorinilja@yandex.ru>
2022-11-06 19:45:43 +0400

    Test

--- src/test/java/AfishaTest.java
+++ src/test/java/AfishaTest.java
@@ -71,9 +71,31 @@ public class AfishaTest {
         afisha.add("Movie 10");
         afisha.add("Movie 11");
 
-        String[] expectual = {"Movie 11","Movie 10","Movie 9","Movie 8","Movie 7","Movie 6","Movie 5","Movie 4","Movie 3","Movie 2"};
+        String[] expectual = {"Movie 11", "Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2"};
         String[] actual = afisha.findLast();
-        Assertions.assertArrayEquals(expectual,actual);
+        Assertions.assertArrayEquals(expectual, actual);
+    }
+
+        @Test
+
+        public void TestEqually () {
+            Afisha afisha = new Afisha(10);
+
+            afisha.add("Movie 1");
+            afisha.add("Movie 2");
+            afisha.add("Movie 3");
+            afisha.add("Movie 4");
+            afisha.add("Movie 5");
+            afisha.add("Movie 6");
+            afisha.add("Movie 7");
+            afisha.add("Movie 8");
+            afisha.add("Movie 9");
+            afisha.add("Movie 10");
+
+
+            String[] expectual = {"Movie 10","Movie 9","Movie 8","Movie 7","Movie 6","Movie 5","Movie 4","Movie 3","Movie 2","Movie 1"};
+            String[] actual = afisha.findLast();
+            Assertions.assertArrayEquals(expectual,actual);
     }
 
 }
